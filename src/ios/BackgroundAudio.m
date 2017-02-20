e2766b4bab8550acc22ed9bfbc21bcbd691dd009
Thomas Hessler <thomas@burnerfitness.com>
2017-02-20 15:46:03 -0500

    all

---
+++ src/ios/BackgroundAudio.m
@@ -0,0 +1,39 @@
+#import "BackgroundAudio.h"
+#import <AVFoundation/AVFoundation.h>
+
+@implementation BackgroundAudio
+
+NSString* theCallbackId;
+
+// this method is executed when the app loads because of the onload param in plugin.xml
+- (void)pluginInitialize {
+  AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+  NSError *setCategoryError = nil;
+  BOOL ok = [audioSession setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError];
+
+  NSLog(@"BackgroundAudio plugin ok? %@", ok ? @"YES" : @"NO");
+  if (!ok) {
+    NSLog(@"BackgroundAudio plugin error: %@", setCategoryError.description);
+  }
+}
+
+- (void) enableBackgroundMusic {
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    NSError *setCategoryError = nil;
+    BOOL methodSuccess = [audioSession setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError];
+    NSLog(@"Enabled background music? %@", methodSuccess ? @"YES": @"NO");
+    if(!methodSuccess) { NSLog(@"Issue with enabling background audio: %@", setCategoryError.description); }
+    
+}
+
+- (void) disableBackgroundMusic {
+
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    NSError *setCategoryError = nil;
+    BOOL methodSuccess = [audioSession setCategory:AVAudioSessionCategoryAmbient error:&setCategoryError];
+    NSLog(@"Disabled background music? %@", methodSuccess ? @"YES": @"NO");
+    if(!methodSuccess) { NSLog(@"Issue with disabling background audio: %@", setCategoryError.description); }
+
+}
+
+@end
