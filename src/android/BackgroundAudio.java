0140e22a163f814f231f291202357e9eb0c9ee98
Kevin Ryan <kevincryan23@gmail.com>
2017-06-21 21:21:59 -0400

    more fixes by thomas

--- src/android/BackgroundAudio.java
+++ src/android/BackgroundAudio.java
@@ -15,9 +15,9 @@ public class BackgroundAudio extends CordovaPlugin {
 	public void enableBackgroundMusic () {
 		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_LOSS);
 		if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-			Log.d('Woo enabled background moosic!', result);
+			Log.d("Woo enabled background moosic!");
 		} else {
-			Log.d('Damn, issue enabling background moosic.');
+			Log.d("Damn, issue enabling background moosic.");
 		}
 	}
 
@@ -26,9 +26,9 @@ public class BackgroundAudio extends CordovaPlugin {
 		//quiet BG moosic
 		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
 		if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-			Log.d('Woo enabled background moosic!', result);
+			Log.d("Woo enabled background moosic!");
 		} else {
-			Log.d('Damn, issue enabling background moosic.');
+			Log.d("Damn, issue enabling background moosic.");
 		}
 	}
 
@@ -37,9 +37,9 @@ public class BackgroundAudio extends CordovaPlugin {
 		//disable bg moosic
 		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
 		if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-			Log.d('Woo enabled background moosic!', result);
+			Log.d("Woo enabled background moosic!");
 		} else {
-			Log.d('Damn, issue enabling background moosic.');
+			Log.d("Damn, issue enabling background moosic.");
 		}
 	}
 }
