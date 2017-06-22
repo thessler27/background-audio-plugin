242b00e11125f78fb86d32207a2047fff23a51d5
Kevin Ryan <kevincryan23@gmail.com>
2017-06-21 21:45:28 -0400

    more fixes by thomas

--- src/android/BackgroundAudio.java
+++ src/android/BackgroundAudio.java
@@ -2,26 +2,29 @@ package com.thessler.backgroundAudioPlugin;
 
 import org.apache.cordova.CordovaPlugin;
 import android.media.AudioManager;
+import android.content.Context;
 
 public class BackgroundAudio extends CordovaPlugin {
 
 	public AudioManager audioMgr;
+	public OnAudioFocusChangeListener changeListener;
 
 	public void pluginInitialize () {
 		audioMgr = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		changeListener = new OnAudioFocusChangeListener() {};
 	}
 
 	public void enableBackgroundMusic () {
-		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_LOSS);
+		int result = audioMgr.requestAudioFocus(changeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_LOSS);
 	}
 
 	public void quietBackgroundMusic () {
 		//quiet BG moosic
-		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
+		int result = audioMgr.requestAudioFocus(changeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
 	}
 
 	public void disableBackgroundMusic () {
 		//disable bg moosic
-		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+		int result = audioMgr.requestAudioFocus(changeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
 	}
 }
