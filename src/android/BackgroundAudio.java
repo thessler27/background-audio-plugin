e747b7598161948a6d2b2e4e4da91561fd572549
Kevin Ryan <kevincryan23@gmail.com>
2017-06-21 21:34:34 -0400

    more fixes by thomas

--- src/android/BackgroundAudio.java
+++ src/android/BackgroundAudio.java
@@ -7,39 +7,21 @@ public class BackgroundAudio extends CordovaPlugin {
 
 	public audioMgr AudioManager;
 
-	protected void pluginInitialize () {
+	public void pluginInitialize () {
 		audioMgr = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
 	}
 
-	@Override
 	public void enableBackgroundMusic () {
 		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_LOSS);
-		if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-			Log.d("Woo enabled background moosic!");
-		} else {
-			Log.d("Damn, issue enabling background moosic.");
-		}
 	}
 
-	@Override
 	public void quietBackgroundMusic () {
 		//quiet BG moosic
 		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
-		if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-			Log.d("Woo enabled background moosic!");
-		} else {
-			Log.d("Damn, issue enabling background moosic.");
-		}
 	}
 
-	@Override
 	public void disableBackgroundMusic () {
 		//disable bg moosic
 		int result = audioMgr.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-		if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-			Log.d("Woo enabled background moosic!");
-		} else {
-			Log.d("Damn, issue enabling background moosic.");
-		}
 	}
 }
