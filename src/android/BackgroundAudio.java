db9893fd94994fa0b8e680afb8475cd2edd50d86
Thomas Hessler <thomas@burnerfitness.com>
2017-06-22 14:07:33 -0400

    more

--- src/android/BackgroundAudio.java
+++ src/android/BackgroundAudio.java
@@ -9,13 +9,39 @@ public class BackgroundAudio extends CordovaPlugin {
 	public AudioManager audioMgr;
 	public AudioManager.OnAudioFocusChangeListener changeListener;
 	public Context mContext;
+	private final static String TAG = "AudioFocus";
 
 	@Override
 	public void pluginInitialize () {
 		mContext = this.cordova.getActivity().getApplicationContext();
 		audioMgr = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
 		changeListener = new AudioManager.OnAudioFocusChangeListener() {
-			public void onAudioFocusChange(int focusChange) {}
+			public void onAudioFocusChange(int focusChange) {
+				switch (focusChange) {
+	                case AudioManager.AUDIOFOCUS_GAIN:
+	                    Log.i(TAG, "AUDIOFOCUS_GAIN");
+	                    //restart/resume your sound
+	                    break;
+	                case AudioManager.AUDIOFOCUS_LOSS:
+	                    Log.e(TAG, "AUDIOFOCUS_LOSS");
+	                    //Loss of audio focus for a long time
+	                    //Stop playing the sound
+	                    break;
+	                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+	                    Log.e(TAG, "AUDIOFOCUS_LOSS_TRANSIENT");
+	                    //Loss of audio focus for a short time
+	                    //Pause playing the sound
+	                    break;
+	                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+	                    Log.e(TAG, "AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
+	                    //Loss of audio focus for a short time.
+	                    //But one can duck. Lower the volume of playing the sound
+	                    break;
+
+	                default:
+	                    //
+	            }
+			}
 		};
 	}
 
